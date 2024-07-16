78c92a6431206e901b191de4b415bf91c6c9c4bd
uuuwiiiiii <nebogatov.roman@mail.ru>
2024-07-16 10:46:36 +0300

    build

--- src/main/java/calc/oop/calculating.java
+++ src/main/java/calc/oop/calculating.java
@@ -1,51 +1,67 @@
 package calc.oop;
 
 public class calculating {
-    private double firstNumber,secondNumber,result;
+    private double firstNumber, secondNumber, result;
     private char operation;
+
     public calculating() {
-        firstNumber=0;
-        secondNumber=0;
-        result=0;
+        firstNumber = 0;
+        secondNumber = 0;
+        result = 0;
     }
 
-public void add(int a,int b,char oper){
-    firstNumber=a;
-    secondNumber=b;
-    operation=oper;
-}
-
-public void printRes(){
-    cal();
-    System.out.print(result);
-}
-
-private void cal(){
-    switch(operation) {
-        case '+': {
-            result = firstNumber + secondNumber;
-            break;
-        }
-        case '-': {
-            result = firstNumber - secondNumber;
-            break;
-        }
-        case '*': {
-            result = firstNumber * secondNumber;
-            break;
-        }
-        case '/': {
-            try {
-                result = firstNumber / secondNumber;
-            }
-            catch(Exception e) {
-                System.out.println("Warning! Exception has been thrown");
-                return;
-            }
-        }
-        default:
-            break;
+    public void add(int a, int b, char oper) {
+        firstNumber = a;
+        secondNumber = b;
+        operation = oper;
     }
-}
+
+    public void printRes() {
+        cal();
+        System.out.print(result);
+    }
+
+    private void plus() {
+        result = firstNumber + secondNumber;
+    }
+
+    private void minus() {
+        result = firstNumber - secondNumber;
+    }
+
+    private void mult() {
+        result = firstNumber * secondNumber;
+    }
+
+    private void devide() {
+        result = firstNumber / secondNumber;
+    }
+
+    private void cal() {
+        switch (operation) {
+            case '+': {
+                plus();
+                break;
+            }
+            case '-': {
+                minus();
+                break;
+            }
+            case '*': {
+                mult();
+                break;
+            }
+            case '/': {
+                try {
+                    devide();
+                } catch (Exception e) {
+                    System.out.println("Warning! Exception has been thrown");
+                    return;
+                }
+            }
+            default:
+                break;
+        }
+    }
 
 }
